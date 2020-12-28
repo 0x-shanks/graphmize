53ed11faa55eebfbcf8a2746022daf448c58fb49
hourglasshoro <hourglasshoro.628@gmail.com>
2020-12-28 17:12:14 +0900

    update: Changed to allow searching by specifying a directory

--- pkg/graph/graph.go
+++ pkg/graph/graph.go
@@ -5,7 +5,9 @@ import (
 	"github.com/hourglasshoro/graphmize/pkg/file"
 	"github.com/pkg/errors"
 	"github.com/spf13/afero"
+	"os"
 	"path"
+	"strings"
 )
 
 type Graph struct {
@@ -38,17 +40,72 @@ func (g *Graph) Marshal() ([]byte, error) {
 }
 
 // Find determines if an element exists in the slice
-func Find(slice []string, val string) bool {
-	for _, item := range slice {
+func Find(slice []string, val string) (bool, int) {
+	for i, item := range slice {
 		if item == val {
-			return true
+			return true, i
 		}
 	}
-	return false
+	return false, -1
 }
 
-// BuildGraph recursively calls resources from the root KustomizationFile to build a Graph
-func BuildGraph(ctx file.Context, directoryPath string, kustomizationFile file.KustomizationFile) (*Graph, error) {
+// BuildGraph recursively explores the specified directory, builds a dependency tree, and returns it
+func BuildGraph(ctx file.Context, rootPath string) (*Graph, error) {
+	rootGraph := NewGraph("root", "root", "/", []Graph{})
+
+	// parentNodes determine if search should be skipped in BuildGraphFromDir
+	parentNodes := map[string]*Graph{}
+
+	// childNodes determine whether to put in parentNode
+	childNodes := map[string]bool{}
+
+	err := afero.Walk(ctx.FileSystem, rootPath,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			fileNameStartIndex := strings.LastIndex(path, "/")
+			// If not rootPath
+			if fileNameStartIndex > 0 {
+
+				// Search kustomizationFile
+				isKustomizationFile, _ := Find(file.KustomizationFileNames, path[fileNameStartIndex+1:])
+
+				if isKustomizationFile {
+					kustomizationFilePath := path[:fileNameStartIndex]
+					kustomizationFile, err := ctx.GetKustomizationFromDirectory(kustomizationFilePath)
+					if err != nil {
+						return errors.Wrap(err, "cannot get kustomization file")
+					}
+
+					graph, err := BuildGraphFromDir(ctx, kustomizationFilePath, *kustomizationFile, parentNodes, childNodes)
+					if err != nil {
+						return errors.Wrap(err, "cannot get graph")
+					}
+
+					// Do not add already explored kustomization files to the parent
+					if _, isChild := childNodes[kustomizationFilePath]; !isChild {
+						parentNodes[kustomizationFilePath] = graph
+					}
+				}
+			}
+
+			return nil
+		})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range parentNodes {
+		rootGraph.Resources = append(rootGraph.Resources, *v)
+	}
+
+	return rootGraph, nil
+}
+
+// BuildGraphFromDir builds and returns a dependency tree from a kustomization file under the specified directory
+func BuildGraphFromDir(ctx file.Context, directoryPath string, kustomizationFile file.KustomizationFile, parentNodes map[string]*Graph, childNodes map[string]bool) (*Graph, error) {
 	var resources []Graph
 	for _, resource := range kustomizationFile.Resources {
 
@@ -67,17 +124,29 @@ func BuildGraph(ctx file.Context, directoryPath string, kustomizationFile file.K
 		}
 		if isDir {
 			// For directories
-			childKustomizationFile, err := ctx.GetKustomizationFromDirectory(resourcePath)
-			if err != nil {
-				return nil, errors.Wrap(err, "cannot get childKustomizationFile")
+
+			graph, isExplored := parentNodes[resourcePath]
+
+			// If a file at this path is already registered as a parent when searching
+			if isExplored {
+				delete(parentNodes, resourcePath)
+			} else {
+				childKustomizationFile, err := ctx.GetKustomizationFromDirectory(resourcePath)
+				if err != nil {
+					return nil, errors.Wrap(err, "cannot get childKustomizationFile")
+				}
+				graph, err = BuildGraphFromDir(ctx, resourcePath, *childKustomizationFile, parentNodes, childNodes)
+				if err != nil {
+					return nil, errors.Wrap(err, "cannot buildGraph for childKustomizationFile")
+				}
 			}
-			graph, err := BuildGraph(ctx, resourcePath, *childKustomizationFile)
-			if err != nil {
-				return nil, errors.Wrap(err, "cannot buildGraph for childKustomizationFile")
-			}
+
 			resources = append(resources, *graph)
 
-		} else if exist := Find(file.KustomizationFileNames, resource); exist {
+			// Register nodes that have already been explored
+			childNodes[resourcePath] = true
+
+		} else if exist, _ := Find(file.KustomizationFileNames, resource); exist {
 			// For kustomizationFile
 			return nil, errors.New("must be a directory")
 		} else {
--- pkg/graph/graph_test.go
+++ pkg/graph/graph_test.go
@@ -1,11 +1,14 @@
 package graph
 
 import (
+	"errors"
 	"github.com/hourglasshoro/graphmize/pkg/file"
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
+
+// BuildGraphFromDir Test
 
 // TestBuildGraphRecursively tests to validate that when tree-structurally dependent
 func TestBuildGraphRecursively(t *testing.T) {
@@ -52,17 +55,17 @@ kind: Deployment
 
 	dir := "app"
 	kustomizationFile, _ := file.NewFromFileSystem(fakeFileSystem).GetKustomizationFromDirectory(dir)
-	graph, err := BuildGraph(*ctx, dir, *kustomizationFile)
-
+	graph, err := BuildGraphFromDir(*ctx, dir, *kustomizationFile, map[string]*Graph{}, map[string]bool{})
 	assert.Nil(t, err)
 
 	expected := "a.yaml"
 	actual := graph.Resources[0].Resources[0].FileName
-	assert.Equal(t, actual, expected)
+	assert.Equal(t, expected, actual)
 
 	expected = "b.yaml"
 	actual = graph.Resources[0].Resources[1].FileName
-	assert.Equal(t, actual, expected)
+	assert.Equal(t, expected, actual)
+
 }
 
 // TestBuildGraphRecursivelyJump tests to validate that specifying dependencies with relative paths
@@ -114,15 +117,163 @@ kind: Deployment
 
 	dir := "app/sub"
 	kustomizationFile, _ := file.NewFromFileSystem(fakeFileSystem).GetKustomizationFromDirectory(dir)
-	graph, err := BuildGraph(*ctx, dir, *kustomizationFile)
+	graph, err := BuildGraphFromDir(*ctx, dir, *kustomizationFile, map[string]*Graph{}, map[string]bool{})
 
 	assert.Nil(t, err)
 
 	expected := "a.yaml"
 	actual := graph.Resources[0].Resources[0].FileName
-	assert.Equal(t, actual, expected)
+	assert.Equal(t, expected, actual)
 
 	expected = "b.yaml"
 	actual = graph.Resources[1].FileName
-	assert.Equal(t, actual, expected)
+	assert.Equal(t, expected, actual)
+
+}
+
+// BuildGraph Test
+
+// TestBuildGraph tests to validate that root directory with no kustomization file
+func TestBuildGraph(t *testing.T) {
+	// Folder structure for this test
+	//
+	//   /app
+	//   |
+	//   ├── base
+	//	 | ├── kustomization.yaml
+	//	 | └── a.yaml
+	//   |
+	//   └── sub
+	//	   ├── kustomization.yaml
+	//	   └── b.yaml
+
+	fake := afero.NewMemMapFs()
+	ctx := file.NewContext(fake)
+	fakeFileSystem := ctx.FileSystem
+	fakeFileSystem.Mkdir("app", 0755)
+	fakeFileSystem.Mkdir("app/base", 0755)
+	fakeFileSystem.Mkdir("app/sub", 0755)
+
+	fileContents := `
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+
+resources:
+- a.yaml
+`
+	afero.WriteFile(fakeFileSystem, "app/base/kustomization.yaml", []byte(fileContents), 0644)
+
+	fileContents = `
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+
+resources:
+- ../base
+- b.yaml
+`
+	afero.WriteFile(fakeFileSystem, "app/sub/kustomization.yaml", []byte(fileContents), 0644)
+
+	fileContents = `
+apiVersion: apps/v1
+kind: Deployment
+`
+	afero.WriteFile(fakeFileSystem, "app/base/a.yaml", []byte(fileContents), 0644)
+	afero.WriteFile(fakeFileSystem, "app/sub/b.yaml", []byte(fileContents), 0644)
+
+	graph, err := BuildGraph(*ctx, "app")
+	assert.Nil(t, err)
+
+	expected := "a.yaml"
+	actual := graph.Resources[0].Resources[0].Resources[0].FileName
+	assert.Equal(t, expected, actual)
+
+	expected = "b.yaml"
+	actual = graph.Resources[0].Resources[1].FileName
+	assert.Equal(t, expected, actual)
+
+}
+
+// TestBuildGraph tests to validate that when you have two independent dependencies
+func TestBuildGraphWithTwoParents(t *testing.T) {
+	// Folder structure for this test
+	//
+	//   /app
+	//   |
+	//   ├── base
+	//	 | ├── kustomization.yaml
+	//	 | └── a.yaml
+	//   |
+	//   └── staging
+	//	 | ├── kustomization.yaml
+	//	 | └── b.yaml
+	//   └── production
+	//	   ├── kustomization.yaml
+	//	   └── c.yaml
+
+	fake := afero.NewMemMapFs()
+	ctx := file.NewContext(fake)
+	fakeFileSystem := ctx.FileSystem
+	fakeFileSystem.Mkdir("app", 0755)
+	fakeFileSystem.Mkdir("app/base", 0755)
+	fakeFileSystem.Mkdir("app/staging", 0755)
+	fakeFileSystem.Mkdir("app/production", 0755)
+
+	fileContents := `
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+
+resources:
+- a.yaml
+`
+	afero.WriteFile(fakeFileSystem, "app/base/kustomization.yaml", []byte(fileContents), 0644)
+
+	fileContents = `
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+
+resources:
+- ../base
+- b.yaml
+`
+	afero.WriteFile(fakeFileSystem, "app/staging/kustomization.yaml", []byte(fileContents), 0644)
+
+	fileContents = `
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+
+resources:
+- ../base
+- c.yaml
+`
+	afero.WriteFile(fakeFileSystem, "app/production/kustomization.yaml", []byte(fileContents), 0644)
+
+	fileContents = `
+apiVersion: apps/v1
+kind: Deployment
+`
+	afero.WriteFile(fakeFileSystem, "app/base/a.yaml", []byte(fileContents), 0644)
+	afero.WriteFile(fakeFileSystem, "app/staging/b.yaml", []byte(fileContents), 0644)
+	afero.WriteFile(fakeFileSystem, "app/production/c.yaml", []byte(fileContents), 0644)
+
+	graph, err := BuildGraph(*ctx, "app")
+	assert.Nil(t, err)
+
+	expected := "a.yaml"
+	actual := graph.Resources[0].Resources[0].Resources[0].FileName
+	assert.Equal(t, expected, actual)
+
+	for _, v := range graph.Resources {
+		switch v.FileName {
+		case "app/staging":
+			expected = "b.yaml"
+			actual = v.Resources[1].FileName
+			assert.Equal(t, expected, actual)
+		case "app/production":
+			expected = "c.yaml"
+			actual = v.Resources[1].FileName
+			assert.Equal(t, expected, actual)
+		default:
+			assert.Error(t, errors.New("invalid File Name"))
+		}
+	}
 }
