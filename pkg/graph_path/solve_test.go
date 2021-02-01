0e86a39cdc97be9eac23959a88a9b951a63be3b8
hourglasshoro <hourglasshoro.628@gmail.com>
2021-02-01 14:55:44 +0900

    add: solve_test.go

---
+++ pkg/graph_path/solve_test.go
@@ -0,0 +1,32 @@
+package graph_path
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// Solve Test
+
+//  TestSolveWithNoSource tests to validate when not specified in the -s option
+func TestSolveWithNoSource(t *testing.T) {
+	source := ""
+	actual := Solve(source, "/User/test//app")
+	expected := "/User/test//app"
+	assert.Equal(t, expected, actual)
+}
+
+//  TestSolveWithNoSource tests to validate when a relative path is specified with the -s option
+func TestSolveWithRelativePath(t *testing.T) {
+	source := "../app2"
+	actual := Solve(source, "/User/test/app1")
+	expected := "/User/test/app2"
+	assert.Equal(t, expected, actual)
+}
+
+//  TestSolveWithNoSource tests to validate when a absolute path is specified with the -s option
+func TestSolveWithAbsolutePath(t *testing.T) {
+	source := "/User/test/app2"
+	actual := Solve(source, "/app1")
+	expected := "/User/test/app2"
+	assert.Equal(t, expected, actual)
+}
