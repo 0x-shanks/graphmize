e17e52fe5410e97c63bd3944f02bb8d2b284c60c
hourglasshoro <hourglasshoro.628@gmail.com>
2021-02-01 15:14:08 +0900

    update: Added description of Solve method

--- pkg/imput/solve.go
+++ pkg/imput/solve.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 )
 
+// Solve resolves the path of the root directory to be searched from the input source and currentDir
 func Solve(source string, currentDir string) (graphDir string) {
 	if source != "" && !filepath.IsAbs(source) {
 		graphDir = path.Join(currentDir, source)
